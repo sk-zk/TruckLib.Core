1db31cee607a341d399cfc0397c7a6e6f78bd4ea
sk-zk <sk-zk@users.noreply.github.com>
2024-10-26 15:35:05 +0200

    Add `IFileSystem`

---
+++ TruckLib.Core/DiskFileSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckLib
+{
+    public class DiskFileSystem : IFileSystem
+    {
+        /// <inheritdoc/>
+        public char DirectorySeparator => Path.DirectorySeparatorChar;
+
+        /// <inheritdoc/>
+        public bool FileExists(string path) => File.Exists(path);
+
+        /// <inheritdoc/>
+        public IList<string> GetFiles(string path) => Directory.GetFiles(path);
+
+        /// <inheritdoc/>
+        public string GetParent(string path) => Directory.GetParent(path).FullName;
+
+        /// <inheritdoc/>
+        public Stream Open(string path) => new FileStream(path, FileMode.Open, FileAccess.Read);
+
+        /// <inheritdoc/>
+        public byte[] ReadAllBytes(string path) => File.ReadAllBytes(path);
+
+        /// <inheritdoc/>
+        public string ReadAllText(string path) => File.ReadAllText(path);
+
+        /// <inheritdoc/>
+        public string ReadAllText(string path, Encoding encoding) => File.ReadAllText(path, encoding);
+    }
+}
